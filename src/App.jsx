e3b13979adeb199da644a94e682fa0d22246e606
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-06-07 10:36:15 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Logos from './Logos'
+import Button from './Button'
+
+function App() {
+  
+
+  return (
+    <>
+      <Logos />
+      <h1>Willkommen zum Wiss Quiz</h1>
+      
+      <div className="card">
+        < Button />
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
